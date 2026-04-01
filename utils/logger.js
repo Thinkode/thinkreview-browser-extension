82e99171a3ada294e9b530d0441fcb226e37c03e
Jay Elsheikh <jay.shenawy@outlook.com>
2026-04-01 02:54:23 +0100

    debug flag off

--- utils/logger.js
+++ utils/logger.js
@@ -5,7 +5,7 @@
 import { reportError, reportMessage, isInitialized } from './honeybadger-service.js';
 
 // Debug toggle: set to false to disable console logs in production
-const DEBUG = true;
+const DEBUG = false;
 
 /**
  * Extract the calling filename from stack trace
